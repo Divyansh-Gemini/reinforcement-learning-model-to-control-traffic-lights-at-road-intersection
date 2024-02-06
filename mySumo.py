4a26244529e2473c86dbc75e0267a022701a69d7
Divyansh Gemini <divyanshgemini3232@gmail.com>
2024-02-06 17:07:31 +0530

    added more vehicle types

--- mySumo.py
+++ mySumo.py
@@ -1,5 +1,6 @@
 import subprocess
 
+# file names
 nodesFile = 'nodes.nod.xml'
 edgesFile = 'edges.edg.xml'
 rightSideNetworkFile = 'rightHandNetwork.net.xml'
@@ -8,25 +9,24 @@ routesFile = 'routes.rou.xml'
 typesFile = 'types.type.xml'
 configFile = 'configuration.sumocfg'
 
+# command to generate network file (.net.xml)
 netConvertCommand = ('netconvert --node-files ' + nodesFile + ' --edge-files ' + edgesFile + ' -t ' + typesFile + ' -o '
                      + rightSideNetworkFile)
 
+# command to open simulation in SUMO
 sumoGUICommand = 'sumo-gui -c ' + configFile
 
+# command to convert network from right-hand side to left-hand side
 rightToLeftConvertCommand = 'netconvert -s ' + rightSideNetworkFile + ' --flip-y-axis -o ' + leftSideNetworkFile
 
-command = input('Which command you want to execute?\n1. netconvert\n2. sumo-gui\n\n>> ')
+command = input('Which command you want to execute?\n1. netconvert\n2. sumo-gui\n>> ')
 
 match command:
     case '1':
-        # generates .net.xml file
         subprocess.run(netConvertCommand)
-
-        # convert .net.xml from right-hand-side to left-hand-side
         subprocess.run(rightToLeftConvertCommand)
 
     case '2':
-        # launch SUMO GUI
         subprocess.run(sumoGUICommand)
 
     case default:
