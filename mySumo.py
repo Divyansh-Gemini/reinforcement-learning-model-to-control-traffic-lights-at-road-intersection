2cf2861d6b308f76fc1fad92a3d44bab244caf3e
Divyansh Gemini <divyanshgemini3232@gmail.com>
2024-02-09 16:09:17 +0530

    randomized depart interval for each trip

--- mySumo.py
+++ mySumo.py
@@ -9,19 +9,23 @@ tripsFile = 'trips.trips.xml'
 modifyTripsScript = 'modifyTrips.py'
 modifiedTripsFile = 'modified_trips.trips.xml'
 routesFile = 'routes.rou.xml'
+sortRandomRoutesScript = 'sortRandomRoutes.py'
 configFile = 'configuration.sumocfg'
 
 # command to generate network file (.net.xml)
 netConvertCommand = f'netconvert --node-files {nodesFile} --edge-files {edgesFile} -o {networkFile} --lefthand'
 
 # generate random trips
-randomTripsCommand = f'python {randomTripsScript} -n {networkFile} -e 200 -o {tripsFile}'
+randomTripsCommand = f'python {randomTripsScript} -n {networkFile} -e 1000 -o {tripsFile}'
 
 # modify trips
 modifyTripsCommand = f'python {modifyTripsScript}'
 
 # generate .rou.xml file
 duarouterCommand = f'duarouter -n {networkFile} -r {modifiedTripsFile} --ignore-errors --no-warnings --begin 0 --end 600 -o {routesFile}'
+
+# sort random routes
+sortRandomCommand = f'python {sortRandomRoutesScript}'
 
 # command to open simulation in SUMO
 sumoGUICommand = f'sumo-gui -c {configFile}'
@@ -36,6 +40,7 @@ match command:
         subprocess.run(randomTripsCommand)
         subprocess.run(modifyTripsCommand)
         subprocess.run(duarouterCommand)
+        subprocess.run(sortRandomCommand)
 
     case '3':
         subprocess.run(sumoGUICommand)
